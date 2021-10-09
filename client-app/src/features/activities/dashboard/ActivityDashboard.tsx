b45d299ac7fa07ab33457e4ee8ed5bc52cf8454e
Deepak Shinde <deepak27shinde@gmail.com>
2021-10-09 20:03:31 +0530

    Section 8 : Cleaning up unused code.

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -16,10 +16,11 @@ import ActivityList from "./ActivityList";
 export default observer(function ActivityDashboard() {
   // mobx store.
   const { activityStore } = useStore();
+  const { loadActivities, activityRegistry } = activityStore;
 
   useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore]);
+    if (activityRegistry.size <= 1) loadActivities();
+  }, [activityRegistry.size, loadActivities]);
 
   if (activityStore.loadingInitial) return <LoadingComponent content='loading app..' />
 
