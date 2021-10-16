caca98debc04174a07508ff53b62927e14247e7b
deepakps <deepak27shinde@gmail.com>
2021-10-16 19:49:39 +0530

    Section 9 : Grouping the activities by date.

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -28,9 +28,25 @@ export default class ActivityStore {
         makeAutoObservable(this);
     }
 
+    // function to sort the activities by dates.
+    // date - 18th Aug, 2021.
     get activitiesByDate() {
         return Array.from(this.activityRegistry.values()).sort((a, b) =>
             Date.parse(a.date) - Date.parse(b.date));
+    }
+
+    // function to sort the activities by dates.
+    // date - 13th Oct, 2021.
+    get groupedActivities() {
+        console.log(this.activitiesByDate.values);
+        return Object.entries(
+            this.activitiesByDate.reduce((activities, activity) => {
+                const date = activity.date;
+                console.log(activities);
+                activities[date] = activities[date] ? [...activities[date], activity] : [activity];
+                return activities;
+            }, {} as { [key: string]: Activity[] })
+        );
     }
 
     setTitle = () => {
